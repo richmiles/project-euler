a9b889ecac53572ec0155dfc523b14dbe2127f30
Rich Miles <me@richmiles.xyz>
2023-12-05 11:08:00 -0700

    Solved problem 19.

---
+++ 0019/Program.cs
@@ -0,0 +1,14 @@
+﻿var sundays = 0;
+for(int year = 1901; year <= 2000; year++)
+{
+    for(int month = 1; month <= 12; month++)
+    {
+        var date = new DateTime(year, month, 1);
+        if(date.DayOfWeek == DayOfWeek.Sunday)
+        {
+            sundays++;
+        }
+    }
+}
+
+Console.WriteLine(sundays);
