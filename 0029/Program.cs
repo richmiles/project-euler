c077e833f0439fd8da0f1bafd2b8570476af9330
Rich Miles <me@richmiles.xyz>
2023-12-05 12:39:58 -0700

    Solved problem 29.

---
+++ 0029/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+var distinctTerms = new HashSet<BigInteger>();
+for (BigInteger a = 2; a <= 100; a++)
+{
+    for (BigInteger b = 2; b <= 100; b++)
+    {
+        var result = BigInteger.Pow(a, (int)b);
+        distinctTerms.Add(result);
+    }
+}
+Console.WriteLine(distinctTerms.Count);
