b7f2f29395231d5ab5de9c7617a9dd88fa921dd1
Rich Miles <me@richmiles.xyz>
2023-12-05 12:46:58 -0700

    Solved problem 30.

---
+++ 0030/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var sumsOfFifthPowers = new List<int>();
+for (int i = 2; i < 1000000; i++)
+{
+    var digits = getDigits(i);
+    var sumOfFifthPowers = 0;
+    for (var digitIndex = 0; digitIndex < digits.Count; digitIndex++)
+    {
+        sumOfFifthPowers += (int)Math.Pow(digits[digitIndex], 5);
+    }
+    if (sumOfFifthPowers == i)
+    {
+        sumsOfFifthPowers.Add(i);
+    }
+}
+
+Console.WriteLine(sumsOfFifthPowers.Sum());
+
+List<int> getDigits(int n)
+{
+    var digits = new List<int>();
+    while (n > 0)
+    {
+        digits.Add(n % 10);
+        n /= 10;
+    }
+    digits.Reverse();
+    return digits;
+}
