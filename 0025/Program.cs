8fece6c2a2b5f8373c1f11d06db8032d9e52cfb9
Rich Miles <me@richmiles.xyz>
2023-12-05 12:03:16 -0700

    Solved problem 25.

---
+++ 0025/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+Console.WriteLine(fibonacci(1000));
+
+//Find the first fibonacci number with 1000 digits
+BigInteger fibonacci(int digits)
+{
+    BigInteger a = 1;
+    BigInteger b = 1;
+    BigInteger c = 0;
+    var index = 2;
+    while (c.ToString().Length < digits)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        index++;
+    }
+    return index;
+}
