67f8c0375e2dc32ac1f2c25267373a51e85afaf2
Rich Miles <me@richmiles.xyz>
2023-12-05 11:09:46 -0700

    Solved problem 20.

---
+++ 0020/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Numerics;
+
+BigInteger result = factorial(100);
+Console.WriteLine(result.ToString().Sum(c => c - '0'));
+
+BigInteger factorial(int n)
+{
+    BigInteger result = 1;
+    for (var i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
