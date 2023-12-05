30908b79e94718bdc7efcbe18d0e150323ebde68
Rich Miles <me@richmiles.xyz>
2023-12-05 12:10:16 -0700

    Solved problem 26.

---
+++ 0026/Program.cs
@@ -0,0 +1,36 @@
+﻿//Find the value of d < 1000 for which contains the longest recurring cycle in its decimal fraction part.
+
+var maxCycleLength = 0;
+for (int d = 1; d < 1000; d++)
+{
+    var cycleLength = getCycleLength(d);
+    if (cycleLength > maxCycleLength)
+    {
+        maxCycleLength = cycleLength;
+        Console.WriteLine($"{d}: {cycleLength}");
+    }
+}
+
+int getCycleLength(int d)
+{
+    var numerator = 1;
+    var numeratorHistory = new List<int>();
+    while (true)
+    {
+        while (numerator < d)
+        {
+            numerator *= 10;
+        }
+        var remainder = numerator % d;
+        if (remainder == 0)
+        {
+            return 0;
+        }
+        if (numeratorHistory.Contains(remainder))
+        {
+            return numeratorHistory.Count - numeratorHistory.IndexOf(remainder);
+        }
+        numeratorHistory.Add(remainder);
+        numerator = remainder;
+    }
+}
